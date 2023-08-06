f554dd7db725e778b1eb7a74700522a0878ead05
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-08-06 12:48:00 -0500

    Added Priority Select

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const taskInput = document.getElementById("task");
 const addButton = document.getElementById("add-btn");
 const todoList = document.getElementById("todo-list");
-
+const prioritySelect = document.getElementById("priority"); // Get the reference to the priority select element
 
 function addTask() {
     const taskText = taskInput.value.trim();
+    const priorityValue = prioritySelect.value; // Get the selected priority value from the select element
+
     if (taskText !== "") {
         const newTask = document.createElement("li");
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete task";
-        
-        newTask.textContent = taskText;
+
+        newTask.textContent = `${taskText} (Priority: ${priorityValue})`; // Add the priority to the task text
         newTask.appendChild(deleteButton);
 
-        
         deleteButton.addEventListener("click", function() {
             newTask.remove();
         });
@@ -23,9 +24,7 @@ function addTask() {
     }
 }
 
-
 addButton.addEventListener("click", addTask);
-
 
 taskInput.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
