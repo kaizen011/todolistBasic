5dc2a9d479d419a8bc75f77a45bd943090db13b1
Kaizen <125158784+kaizen011@users.noreply.github.com>
2023-08-07 06:55:15 -0500

    Added Edit feature

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const taskInput = document.getElementById("task");
 const addButton = document.getElementById("add-btn");
 const todoList = document.getElementById("todo-list");
-const prioritySelect = document.getElementById("priority"); // Get the reference to the priority select element
+const prioritySelect = document.getElementById("priority");
 
 function addTask() {
     const taskText = taskInput.value.trim();
-    const priorityValue = prioritySelect.value; // Get the selected priority value from the select element
+    const priorityValue = prioritySelect.value;
 
     if (taskText !== "") {
         const newTask = document.createElement("li");
+        newTask.textContent = `${taskText} (Priority: ${priorityValue})`;
+
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete task";
-
-        newTask.textContent = `${taskText} (Priority: ${priorityValue})`; // Add the priority to the task text
-        newTask.appendChild(deleteButton);
-
         deleteButton.addEventListener("click", function() {
             newTask.remove();
         });
 
+        const editButton = document.createElement("button");
+        editButton.textContent = "Edit task";
+        editButton.addEventListener("click", function() {
+            const editInput = document.createElement("input");
+            editInput.value = taskText;
+            
+            const saveButton = document.createElement("button");
+            saveButton.textContent = "Save";
+            saveButton.addEventListener("click", function() {
+                const editedText = editInput.value;
+                newTask.textContent = `${editedText} (Priority: ${priorityValue})`;
+                newTask.appendChild(deleteButton);
+                newTask.appendChild(editButton);
+            });
+
+            newTask.textContent = "";
+            newTask.appendChild(editInput);
+            newTask.appendChild(saveButton);
+        });
+
+        newTask.appendChild(deleteButton);
+        newTask.appendChild(editButton);
         todoList.appendChild(newTask);
+
         taskInput.value = "";
     }
 }
